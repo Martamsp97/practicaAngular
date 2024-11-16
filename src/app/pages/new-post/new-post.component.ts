01c1a35dfb2fb3c872d621cd9edd2b80c3d09cfb
Marta Sánchez <marta.msp97@gmail.com>
2024-11-16 22:33:29 +0100

    instalado sweet alert, automatizada la fecha, conectado con el servicio y realizadas las validaciones

--- src/app/pages/new-post/new-post.component.ts
+++ src/app/pages/new-post/new-post.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PostsService } from '../../services/posts.service';
+import { IPosts } from '../../interfaces/iposts.interface';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-new-post',
@@ -9,9 +13,12 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './new-post.component.css'
 })
 export class NewPostComponent {
-  
- 
-  newProduct: FormGroup = new FormGroup({
+
+  PostsServicio = inject(PostsService)
+
+  router = inject(Router)
+
+  newPost: FormGroup = new FormGroup({
     titulo: new FormControl(null, [
       Validators.required
     ]),
@@ -27,10 +34,21 @@ export class NewPostComponent {
   })
 
   onSubmit() {
-    if (this.newProduct.valid) {
-      console.log(this.newProduct.value);
+    if (this.newPost.valid) {
+      const nuevoPost: IPosts = {
+        ...this.newPost.value,
+        _id: '',
+        fecha: new Date()
+      };
+
+      this.PostsServicio.createPost(this.newPost.value)
+      console.log(nuevoPost)
+      Swal.fire({ title: 'Nueva publicación', text: '¡Publicación creada con éxito!', icon: 'success' })
+      this.router.navigateByUrl('')
+
     }
+    
   }
-  }
-  
+}
+
 
