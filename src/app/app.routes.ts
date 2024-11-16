fc843bfc21b7cb9f2457018051b5408215cf7ddd
Marta Sánchez <marta.msp97@gmail.com>
2024-11-16 19:21:03 +0100

    Pasos avanzados en la aplicación: Se han enlazado las rutas a los diferentes componentes además de realizar un comentaio en ** de recordatorio para generar, en un futuro, un componente Error404. Se ha definido la interfaz IPost{titulo, texto, autor, imagen, fecha, categoria) En servicio: Se ha creado y definifo el array posts Se han creado los métodos: getAll(), createPost(), getByCategory()

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListaPostComponent } from './pages/lista-post/lista-post.component';
+import { NewPostComponent } from './pages/new-post/new-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: ListaPostComponent },
+    { path: "new", component: NewPostComponent },
+    {path:"**", redirectTo:""} //queda a la espera de crear componente error404
+];
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { IPosts } from '../interfaces/iposts.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  private posts: IPosts[] = []
+
+  getAll() {
+    return this.posts
+  }
+  createPost(newPost: IPosts) {
+    this.posts.push(newPost)
+  }
+  getByCategory(categoria: string): IPosts[] {
+    return this.posts.filter(post => post.categoria === categoria);
+  }
 }
