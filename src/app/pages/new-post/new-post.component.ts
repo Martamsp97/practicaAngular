e46ef5bf01888c0547f8ed66305a006a6a211b58
Marta Sánchez <marta.msp97@gmail.com>
2024-11-16 21:05:54 +0100

    No funcionaba el bootstrap, se ha resuelto. El formulario funciona

--- src/app/pages/new-post/new-post.component.ts
+++ src/app/pages/new-post/new-post.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-post',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-post.component.html',
   styleUrl: './new-post.component.css'
 })
 export class NewPostComponent {
+  
+ 
+  newProduct: FormGroup = new FormGroup({
+    titulo: new FormControl(null, [
+      Validators.required
+    ]),
+    texto: new FormControl(null, [
+      Validators.required
+    ]),
+    autor: new FormControl(),
+    imagen: new FormControl(),
+    fecha: new FormControl(),
+    categoria: new FormControl('', [
+      Validators.required
+    ])
+  })
 
-}
+  onSubmit() {
+    if (this.newProduct.valid) {
+      console.log(this.newProduct.value);
+    }
+  }
+  }
+  
+
